9584e999b0cf7bbe0a43e3a63feaed8dd042fee5
Shawn M. Crawford <sleepy3d@gmail.com>
2019-08-16 13:28:13 -0400

    Initial Commit

---
+++ SegaSaturnIsoAnalyzer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sega Saturn Iso Analyzer / Patcher")]
+[assembly: AssemblyDescription("An analyzer/patcher for Sega Saturn iso's.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shawn M. Crawford [sleepy]")]
+[assembly: AssemblyProduct("SegaSaturnIsoAnalyzerPatcher")]
+[assembly: AssemblyCopyright("Copyright © Shawn M. Crawford [sleepy] 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c2103dd-64b9-43ea-979c-fd198f84ddd7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
